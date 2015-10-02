36b09c27f2bf927c877ed921dd9540faf9c518b5
maxbittker <maxbittker@gmail.com>
2015-10-02 13:44:22 -0400

    net structure:

--- nets.js
+++ nets.js
@@ -4,31 +4,40 @@ var _ = require('underscore')
 
 
 
-function perceptron() {
-    this.inputs = [1, 1]
-    this.weights = [.4, 5]
-    this.output = 0
+function perceptron(inputs, weights, threshold) {
+    this.inputs = inputs
+    this.weights = weights
+    this.threshold = threshold;
+    this.output = -1
 }
 
 perceptron.prototype = {
     update: function() {
-        this.output =
+        var sum =
             _.reduce(_.map(this.inputs, (num, key) => {
                 return num * this.weights[key]
             }, this), (sum, input) => {
                 return sum + input
             })
-        console.log(this.output)
+
+        this.output = (sum >= this.threshold) ? 1 : -1
+            // console.log(this.output)
         return this.output
     }
 }
 
-function network(inputs) {
-    this.inputs = []
-    this.layers = [
-        [new perceptron(), new perceptron()]
+function network() {
+    this.inputs = [0, 0]
+    this.layers = []
+    this.layer0 = [
+        new perceptron(this.inputs, [0, -1], -4),
+        new perceptron(this.inputs, [2, 1], 8),
+        new perceptron(this.inputs, [-2, 1], -8),
+        new perceptron(this.inputs, [3, 1], 13),
+        new perceptron(this.inputs, [-3, 1], -13)
     ]
-    this.outputs = []
+    this.layer1 = [new perceptron([this.layer[0][1].output, this.layers[0][2].output], [1, 1], 2)]
+    this.output = -1
 
 }
 
@@ -46,10 +55,32 @@ network.prototype = {
 
     }
 }
+var n1 = new network()
 
 
-var n1 = new network(0)
+console.log(
+    n1.calculate(4, 3)
+)
 
-n1.calculate(1, 1)
+// var p0 = new perceptron([0, -1], -4)
+// for (var i = -4; i < 6; i++) {
+//     p0.inputs = [0, i]
+//     console.log(i + ":" + p0.update())
+// };
 
-console.log()
+// var p1 = new perceptron([2, 1], 8)
+// for (var i = -4; i < 10; i++) {
+//     p1.inputs = [4, i]
+//     console.log(i + ":" + p1.update())
+// };
+
+// var p2 = new perceptron([-2, 1], -8)
+// for (var i = -4; i < 10; i++) {
+//     p2.inputs = [i, 4]
+//     console.log(i + ":" + p2.update())
+// };
+// var p3 = new perceptron([-3, 1], -13)
+// for (var i = -4; i < 10; i++) {
+//     p3.inputs = [i, 4]
+//     console.log(i + ":" + p3.update())
+// };
