4626b842b26321e697ba155d63e2fed69497f6c8
maxbittker <maxbittker@gmail.com>
2015-10-19 16:47:31 -0400

    annotated

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
+#Max Bittker CMPE 452 Assignment 2
+
+This assignment was completed in javascript.
+./js/A2.js is the annotated source code. 
+To run this code, open A2.html in a modern web browser, or open
+[maxbittker.github.io/NeuralNets/A2.html](http://maxbittker.github.io/NeuralNets/A2.html)
+The black squares on the grid correspond to "11" output values, white squares are "00"
+
+Initial Weights were arbitrarily -1 for both learning methods. For learning rate, a value plus decay of (1/current epoch) was used, for example: 1, .5, .3333, …
+
+Simple feedback learning:
+23 training epochs and 58 milliseconds were required to reach 0 error.
+Final weights:
+ 25.31 ,14.55 ,-2.3
+-26.73 ,16.20 ,-2.04 
+
+Error Correction learning:
+9 training epochs and 34 milliseconds were required to reach 0 error.
+Final weights:
+ 51.01, 29.11 ,-3.66
+-57.01, 34.98 ,-3.22
+
+feel free to email me (netID: 11mb119) with questions if you have any questions or difficulty running this code. 
+
+ What parameter can you use to model the learning for
+the following two types of players? Why? Explain.
+a) What if the player is impatient and restless?
+	This would correspond to a higher learning rate and possible overcorrecting.
+b) What if the player is very focused and attentive?
+	This would correspond to a lower learning rate and possible longer training time.
+
+
+
 #Max Bittker CMPE 452 Assignment 1
 
 This assignment was completed in javascript.
--- js/A2.js
+++ js/A2.js
@@ -1,19 +1,34 @@
 var _ = require('underscore') //underscore is a functional toolkit.e.g. _.exampleUnderscoreFunction
 var trainingData = require('./trainingData.js')
 
-//each perceptron has an input array, a weight array, and a threshold value
+
+/*
+    This program calls simulates a neural network by building and simulating
+    perceptrons layer by layer. It uses 5 input nodes, two hidden layers 
+    and one output node to arive at a conclusion: -1 for outside the polygon,
+    1 for inside the polygon. Each node has individually chosen weights and 
+    threshhold values.
+
+    It runs this simulation once for each square of the grid and prints a 
+    unicode character corresponding to the binary output.
+
+    It also runs the simulation in response to the html button and input fields.
+*/
+//initweight is called for initial weight values when instantiationg perceptrons
+//It originaly returned random values, but this made it harder to compare learning methods  
 function initweight() {
-    // return .5
-    return ((Math.random() * 2) - 1);
+    return -1
+        // return ((Math.random() * 2) - 1);
 }
 
+//each perceptron has a learning rate and a weight array, the last value of which is the threshold value
 function perceptron() {
     this.weights = [initweight(), initweight(), initweight()];
     this.lRate = .1
 }
 
-perceptron.prototype = {
-
+perceptron.prototype = { //methods attached to perceptrons:
+    //calculate returns -1 or 1 for a given input co-ordinate
     calc: function(x, y) {
         var sum =
             _.reduce(_.map([x, y, 1], (input, key) => {
@@ -28,16 +43,22 @@ perceptron.prototype = {
         //and sets the output to 1 or -1 
         return (sum > 0) ? 1 : -1
     },
+    //given a learning method, a point, and its correct classification,
+    //learn adjusts the weight values of the perceptron
     learn: function(method, x, y, d) {
-        if (method === "Simple Feedback")
+        if (method == "Simple Feedback") {
             this.learnSimpleFeedback(x, y, d)
-        else
+        } else {
             this.learnErrorCorrection(x, y, d)
+        }
     },
+    //simple feedback learning algorithm implementation
+    //_.map() takes an array (ARG1) and returns an array with the results of calling a function (ARG2)
+    //for each respective element  eg _.map([1,2,3],(x)=>{return(x*2)}) yields [2,4,6]
     learnSimpleFeedback: function(x, y, d) {
         var xi = [x, y, 1];
         if (d === this.calc(x, y)) {
-
+            //do nothing
         } else if (d < this.calc(x, y)) {
             this.weights = _.map(this.weights, (weight, index) => {
                 return weight - (xi[index] * this.lRate)
@@ -48,6 +69,7 @@ perceptron.prototype = {
             })
         }
     },
+    //simple feedback learning algorithm implementation
     learnErrorCorrection: function(x, y, d) {
         var xi = [x, y, 1];
         this.weights = _.map(this.weights, (weight, index) => {
@@ -58,7 +80,7 @@ perceptron.prototype = {
     }
 }
 
-
+//test takes a datapoint, and returns true if the current network classifies it correctly
 function test(data) {
     var dvector = parseD(data[2])
 
@@ -68,8 +90,8 @@ function test(data) {
         output[i] = network[i].calc(data[0], data[1])
     };
 
-    if (output[0] + output[1] === -2) {
-        if (data[0] > 0)
+    if (output[0] + output[1] === -2) { //this deals with the edge case where both nodes return -1
+        if (data[0] >= -.1)
             output[0] = 1
         else
             output[1] = 1
@@ -78,12 +100,10 @@ function test(data) {
     if (output[0] === dvector[0] && output[1] === dvector[1]) {
         return true
     }
-
-    // console.log(output + "|" + dvector)
     return false
-
 }
 
+//parses the result element of the supplied training data into the expected results from the individual perceptrons
 function parseD(d) {
     switch (d) {
         case "-1":
@@ -97,6 +117,7 @@ function parseD(d) {
     }
 }
 
+//train sets learning rate and then calls learn() for each point in the training dataset 
 function train(learningRate, method) {
 
     network[0].lRate = learningRate
@@ -104,6 +125,7 @@ function train(learningRate, method) {
 
     _.each(trainingData, function(data) {
         var dvector = parseD(data[2])
+            //this logic avoids calling perceptron.train on datapoints that don't help apply to it   
         switch (data[2]) {
             case "-1":
                 network[0].learn(method, data[0], data[1], dvector[0])
@@ -121,27 +143,38 @@ function train(learningRate, method) {
     })
 
 }
-
+//a global structure to keep track of current active perceptrons
 var network = [new perceptron(), new perceptron()];
 
+//experiment trains and tests networks iterativly with a given training method. once error is 0, it records some information out to the html
 function experiment(method) {
     document.write('<h4>' + method + ':</h4>');
     var ErrorProgress = ""
     network = [new perceptron(), new perceptron()];
 
-    for (var i = 1; i < 15; i++) {
-        train(.2 / i, method)
+    var start = new Date();
+    for (var i = 1; i < 100; i++) {
+        train(1 / i, method)
         ErrorProgress += ("iteration: " + i + " error:" + (trainingData.length - _.filter(trainingData, test).length) + "<br> ")
+        if (_.filter(trainingData, test).length == trainingData.length)
+            break
     }
+    var finish = new Date();
+    var difference = new Date();
+    difference.setTime(finish.getTime() - start.getTime());
+
 
     render(network)
     document.write(ErrorProgress);
     document.write('<h4> Final Score: ' + (_.filter(trainingData, test).length + '/' + trainingData.length) + '</h4>')
+    document.write('<h4> Final weights: ' + network[0].weights + '<br>' + network[1].weights + '</h4>')
+    document.write('Took ' + difference.getMilliseconds() + "ms")
 }
+
 experiment("Simple Feedback")
 experiment("Error Correction")
 
-
+//render writes out a grid of values with different unicode values correspoding to different output values
 function render(network) {
     document.write('<p class="display">');
 
@@ -160,16 +193,4 @@ function render(network) {
     }
     document.write("</p>");
     document.write("<br>");
-}
-
-
-//this a helper function to parse url parameters
-function getJsonFromUrl() {
-    var query = location.search.substr(1);
-    var result = {};
-    query.split("&").forEach(function(part) {
-        var item = part.split("=");
-        result[item[0]] = decodeURIComponent(item[1]);
-    });
-    return result;
 }
