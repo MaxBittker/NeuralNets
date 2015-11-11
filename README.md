d9e3e6a24ea4ac21309615e0295f69c967baa287
maxbittker <maxbittker@gmail.com>
2015-11-10 22:53:25 -0500

    working with report, no momentum yet

--- README.md
+++ README.md
@@ -1,3 +1,195 @@
+#Max Bittker CMPE 452 Assignment 3
+
+This assignment was completed in javascript.
+./js/A3.js is the annotated source code. 
+
+To run this code, open A3.html in a modern web browser, or open
+[maxbittker.github.io/NeuralNets/A3.html](http://maxbittker.github.io/NeuralNets/A3.html)
+
+please keep in mind that the page may hang/be unresponsive for 1-10 seconds on opening, press "wait", let the script finish. If I had more time I would have made the training asynchonous so that the page didn't hang like this.
+
+###Criteria to stop the training (error, # of iterations):
+Training stops after all points are classified correctly, or 500 epochs.
+
+###Initial weights and learning rate:
+Initial weights are random values between -1 and 1. Learning rate starts at 1 and decays by .2% per epoch
+M value for sigmoid function effects how our network deals with different ranges of input variables. If inputs are normalized, this is easier to decide on
+
+###Number of layers and nodes,
+the network has 4 input nodes, 6 hidden nodes (4 choose 2), and 3 output nodes
+
+###Momentum
+
+###Regularization:
+5% randomization is added to datapoints before training to avoid overfitting
+
+
+###Splitting the given data into training and test data:
+The entire dataset is used for training and testing.
+
+
+
+##Output Table
+
+i: 	   iteration
+Lrate: learning rate (decays)
+error: #of incrorectly classified datapoints
+MSE:   Mean Squared Error 
+ΔMSE:  delta MSE 
+
+i: 0  Lrate: 1.00 error: 100 MSE: 3.372 ΔMSE: 0.76721
+i: 10 Lrate: 0.98 error: 100 MSE: 3.359 ΔMSE: -0.00103
+i: 20 Lrate: 0.96 error: 100 MSE: 3.327 ΔMSE: -0.00318
+i: 30 Lrate: 0.94 error: 45 MSE: 3.185 ΔMSE: -0.02052
+i: 40 Lrate: 0.92 error: 32 MSE: 3.122 ΔMSE: -0.00212
+i: 50 Lrate: 0.90 error: 26 MSE: 3.108 ΔMSE: -0.00098
+i: 60 Lrate: 0.89 error: 22 MSE: 3.100 ΔMSE: -0.00069
+i: 70 Lrate: 0.87 error: 22 MSE: 3.094 ΔMSE: -0.00051
+i: 80 Lrate: 0.85 error: 20 MSE: 3.090 ΔMSE: -0.00036
+i: 90 Lrate: 0.83 error: 20 MSE: 3.087 ΔMSE: -0.00025
+i: 100 Lrate: 0.82 error: 19 MSE: 3.085 ΔMSE: -0.00016
+i: 110 Lrate: 0.80 error: 19 MSE: 3.084 ΔMSE: -0.00011
+i: 120 Lrate: 0.78 error: 19 MSE: 3.083 ΔMSE: -0.00007
+i: 130 Lrate: 0.77 error: 19 MSE: 3.083 ΔMSE: -0.00004
+i: 140 Lrate: 0.75 error: 19 MSE: 3.082 ΔMSE: -0.00002
+i: 150 Lrate: 0.74 error: 19 MSE: 3.082 ΔMSE: -0.00000
+i: 160 Lrate: 0.72 error: 19 MSE: 3.082 ΔMSE: 0.00001
+i: 170 Lrate: 0.71 error: 19 MSE: 3.082 ΔMSE: 0.00001
+i: 180 Lrate: 0.70 error: 19 MSE: 3.083 ΔMSE: 0.00001
+i: 190 Lrate: 0.68 error: 19 MSE: 3.083 ΔMSE: 0.00001
+i: 200 Lrate: 0.67 error: 19 MSE: 3.083 ΔMSE: 0.00000
+i: 210 Lrate: 0.66 error: 19 MSE: 3.083 ΔMSE: -0.00000
+i: 220 Lrate: 0.64 error: 19 MSE: 3.083 ΔMSE: -0.00000
+i: 230 Lrate: 0.63 error: 18 MSE: 3.083 ΔMSE: -0.00000
+i: 240 Lrate: 0.62 error: 18 MSE: 3.083 ΔMSE: -0.00000
+i: 250 Lrate: 0.61 error: 18 MSE: 3.083 ΔMSE: -0.00000
+i: 260 Lrate: 0.59 error: 18 MSE: 3.083 ΔMSE: -0.00000
+i: 270 Lrate: 0.58 error: 18 MSE: 3.083 ΔMSE: 0.00000
+i: 280 Lrate: 0.57 error: 17 MSE: 3.083 ΔMSE: 0.00001
+i: 290 Lrate: 0.56 error: 17 MSE: 3.083 ΔMSE: 0.00001
+i: 300 Lrate: 0.55 error: 17 MSE: 3.083 ΔMSE: 0.00001
+i: 310 Lrate: 0.54 error: 17 MSE: 3.083 ΔMSE: 0.00001
+i: 320 Lrate: 0.53 error: 17 MSE: 3.083 ΔMSE: 0.00001
+i: 330 Lrate: 0.52 error: 17 MSE: 3.083 ΔMSE: 0.00001
+i: 340 Lrate: 0.51 error: 17 MSE: 3.083 ΔMSE: 0.00002
+i: 350 Lrate: 0.50 error: 17 MSE: 3.083 ΔMSE: 0.00002
+i: 360 Lrate: 0.49 error: 17 MSE: 3.084 ΔMSE: 0.00002
+i: 370 Lrate: 0.48 error: 17 MSE: 3.084 ΔMSE: 0.00002
+i: 380 Lrate: 0.47 error: 17 MSE: 3.084 ΔMSE: 0.00002
+i: 390 Lrate: 0.46 error: 17 MSE: 3.084 ΔMSE: 0.00002
+i: 400 Lrate: 0.45 error: 17 MSE: 3.084 ΔMSE: 0.00002
+i: 410 Lrate: 0.44 error: 17 MSE: 3.085 ΔMSE: 0.00002
+i: 420 Lrate: 0.43 error: 17 MSE: 3.085 ΔMSE: 0.00002
+i: 430 Lrate: 0.42 error: 17 MSE: 3.085 ΔMSE: 0.00002
+i: 440 Lrate: 0.41 error: 17 MSE: 3.085 ΔMSE: 0.00002
+i: 450 Lrate: 0.41 error: 17 MSE: 3.085 ΔMSE: 0.00002
+i: 460 Lrate: 0.40 error: 17 MSE: 3.086 ΔMSE: 0.00002
+i: 470 Lrate: 0.39 error: 17 MSE: 3.086 ΔMSE: 0.00002
+i: 480 Lrate: 0.38 error: 17 MSE: 3.086 ΔMSE: 0.00001
+i: 490 Lrate: 0.37 error: 17 MSE: 3.086 ΔMSE: 0.00001
+i: 500 Lrate: 0.37 error: 17 MSE: 3.086 ΔMSE: 0.00001
+i: 510 Lrate: 0.36 error: 17 MSE: 3.086 ΔMSE: 0.00000
+i: 520 Lrate: 0.35 error: 17 MSE: 3.086 ΔMSE: -0.00001
+i: 530 Lrate: 0.35 error: 17 MSE: 3.086 ΔMSE: -0.00002
+i: 540 Lrate: 0.34 error: 17 MSE: 3.086 ΔMSE: -0.00003
+i: 550 Lrate: 0.33 error: 17 MSE: 3.085 ΔMSE: -0.00005
+i: 560 Lrate: 0.33 error: 17 MSE: 3.085 ΔMSE: -0.00008
+i: 570 Lrate: 0.32 error: 17 MSE: 3.084 ΔMSE: -0.00011
+i: 580 Lrate: 0.31 error: 17 MSE: 3.082 ΔMSE: -0.00016
+i: 590 Lrate: 0.31 error: 17 MSE: 3.080 ΔMSE: -0.00022
+i: 600 Lrate: 0.30 error: 17 MSE: 3.078 ΔMSE: -0.00024
+i: 610 Lrate: 0.29 error: 17 MSE: 3.076 ΔMSE: -0.00012
+i: 620 Lrate: 0.29 error: 17 MSE: 3.075 ΔMSE: -0.00014
+i: 630 Lrate: 0.28 error: 17 MSE: 3.074 ΔMSE: -0.00011
+i: 640 Lrate: 0.28 error: 17 MSE: 3.073 ΔMSE: -0.00007
+i: 650 Lrate: 0.27 error: 17 MSE: 3.072 ΔMSE: -0.00004
+i: 660 Lrate: 0.27 error: 17 MSE: 3.072 ΔMSE: -0.00003
+i: 670 Lrate: 0.26 error: 17 MSE: 3.072 ΔMSE: -0.00002
+i: 680 Lrate: 0.26 error: 17 MSE: 3.071 ΔMSE: -0.00001
+i: 690 Lrate: 0.25 error: 17 MSE: 3.071 ΔMSE: -0.00001
+i: 700 Lrate: 0.25 error: 17 MSE: 3.071 ΔMSE: -0.00000
+i: 710 Lrate: 0.24 error: 17 MSE: 3.071 ΔMSE: -0.00000
+i: 720 Lrate: 0.24 error: 17 MSE: 3.071 ΔMSE: -0.00000
+i: 730 Lrate: 0.23 error: 17 MSE: 3.071 ΔMSE: -0.00000
+i: 740 Lrate: 0.23 error: 17 MSE: 3.071 ΔMSE: -0.00000
+i: 750 Lrate: 0.22 error: 17 MSE: 3.071 ΔMSE: 0.00000
+i: 760 Lrate: 0.22 error: 17 MSE: 3.071 ΔMSE: 0.00000
+i: 770 Lrate: 0.21 error: 17 MSE: 3.071 ΔMSE: 0.00000
+i: 780 Lrate: 0.21 error: 17 MSE: 3.071 ΔMSE: 0.00000
+i: 790 Lrate: 0.21 error: 17 MSE: 3.071 ΔMSE: 0.00000
+i: 800 Lrate: 0.20 error: 17 MSE: 3.071 ΔMSE: 0.00000
+i: 810 Lrate: 0.20 error: 17 MSE: 3.071 ΔMSE: 0.00000
+i: 820 Lrate: 0.19 error: 17 MSE: 3.071 ΔMSE: 0.00000
+i: 830 Lrate: 0.19 error: 17 MSE: 3.071 ΔMSE: 0.00000
+i: 840 Lrate: 0.19 error: 17 MSE: 3.071 ΔMSE: 0.00000
+i: 850 Lrate: 0.18 error: 17 MSE: 3.071 ΔMSE: 0.00000
+i: 860 Lrate: 0.18 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 870 Lrate: 0.17 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 880 Lrate: 0.17 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 890 Lrate: 0.17 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 900 Lrate: 0.16 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 910 Lrate: 0.16 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 920 Lrate: 0.16 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 930 Lrate: 0.16 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 940 Lrate: 0.15 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 950 Lrate: 0.15 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 960 Lrate: 0.15 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 970 Lrate: 0.14 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 980 Lrate: 0.14 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 990 Lrate: 0.14 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 1000 Lrate: 0.13 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 1010 Lrate: 0.13 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 1020 Lrate: 0.13 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 1030 Lrate: 0.13 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 1040 Lrate: 0.12 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 1050 Lrate: 0.12 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 1060 Lrate: 0.12 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 1070 Lrate: 0.12 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 1080 Lrate: 0.11 error: 17 MSE: 3.072 ΔMSE: 0.00000
+i: 1090 Lrate: 0.11 error: 17 MSE: 3.073 ΔMSE: 0.00000
+i: 1100 Lrate: 0.11 error: 17 MSE: 3.073 ΔMSE: 0.00000
+i: 1110 Lrate: 0.11 error: 17 MSE: 3.073 ΔMSE: 0.00000
+i: 1120 Lrate: 0.11 error: 16 MSE: 3.073 ΔMSE: 0.00000
+i: 1130 Lrate: 0.10 error: 16 MSE: 3.073 ΔMSE: 0.00000
+i: 1140 Lrate: 0.10 error: 16 MSE: 3.073 ΔMSE: 0.00000
+i: 1150 Lrate: 0.10 error: 16 MSE: 3.073 ΔMSE: 0.00000
+i: 1160 Lrate: 0.10 error: 16 MSE: 3.073 ΔMSE: 0.00000
+i: 1170 Lrate: 0.10 error: 16 MSE: 3.073 ΔMSE: 0.00000
+i: 1180 Lrate: 0.09 error: 16 MSE: 3.073 ΔMSE: 0.00000
+i: 1190 Lrate: 0.09 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1200 Lrate: 0.09 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1210 Lrate: 0.09 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1220 Lrate: 0.09 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1230 Lrate: 0.09 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1240 Lrate: 0.08 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1250 Lrate: 0.08 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1260 Lrate: 0.08 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1270 Lrate: 0.08 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1280 Lrate: 0.08 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1290 Lrate: 0.08 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1300 Lrate: 0.07 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1310 Lrate: 0.07 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1320 Lrate: 0.07 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1330 Lrate: 0.07 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1340 Lrate: 0.07 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1350 Lrate: 0.07 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1360 Lrate: 0.07 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1370 Lrate: 0.06 error: 15 MSE: 3.073 ΔMSE: 0.00000
+i: 1380 Lrate: 0.06 error: 15 MSE: 3.074 ΔMSE: 0.00000
+i: 1390 Lrate: 0.06 error: 15 MSE: 3.074 ΔMSE: 0.00000
+i: 1400 Lrate: 0.06 error: 15 MSE: 3.074 ΔMSE: 0.00000
+i: 1410 Lrate: 0.06 error: 15 MSE: 3.074 ΔMSE: 0.00000
+i: 1420 Lrate: 0.06 error: 15 MSE: 3.074 ΔMSE: 0.00000
+i: 1430 Lrate: 0.06 error: 15 MSE: 3.074 ΔMSE: 0.00000
+i: 1440 Lrate: 0.06 error: 15 MSE: 3.074 ΔMSE: 0.00000
+i: 1450 Lrate: 0.05 error: 15 MSE: 3.074 ΔMSE: 0.00000
+i: 1460 Lrate: 0.05 error: 15 MSE: 3.074 ΔMSE: 0.00000
+i: 1470 Lrate: 0.05 error: 15 MSE: 3.074 ΔMSE: 0.00000
+i: 1480 Lrate: 0.05 error: 15 MSE: 3.074 ΔMSE: 0.00000
+i: 1490 Lrate: 0.05 error: 15 MSE: 3.074 ΔMSE: 0.00000
+
+After 1500 iterations, we correctly classify 135 out of 150 datapoints
+
 #Max Bittker CMPE 452 Assignment 2
 
 This assignment was completed in javascript.
--- js/A3.js
+++ js/A3.js
@@ -1,37 +1,16 @@
 // CISC/CMPE452/COGS400
 // Assignment 3
 // Backpropagation
-
-// Criteria to stop the training (error, # of iterations),
-
-// Initial weights and learning rate,
-
-// Number of layers and nodes,
-
-// Momentum,
-
-// Regularization,
-
-// Splitting the given data into training and test data.
-
-// Print a performance table containing sum of squared error (for all data),
-// weights, classification results (how many and correct/incorrect for each
-// class) and explain your results.
-// at the start
-// at the middle of the training period and end of an epoch (at least one)
-// At the end after training and testing is done
-
 var _ = require('underscore') //underscore is a functional toolkit.e.g. _.exampleUnderscoreFunction
 var trainingData = require('./trainingDataA3.js') //trainingdata.js holds an array of training data
 
 /*
-    This program calls simulates a neural network by with two linear classifier nodes.
-    The networks is trained untill it classifies all training points correctly,
-    and writes then writes output to html.   
+    This program calls simulates a 6,3 backpropogation neural network 
+    The networks is trained untill it classifies all training points correctly, or it reaches a certain number of iterations 
+    and writes then writes progress to html.   
 */
 
-//initweight is called for initial weight values when instantiationg perceptrons
-//It originaly returned     om values, but this made it harder to compare learning methods  
+//initweight is called for initial weight values when instantiationg perceptrons. Each value starts between -1 and 1
 function initweight() {
     return ((Math.random() * 2) - 1);
 }
@@ -39,162 +18,155 @@ function initweight() {
 //each perceptron has a learning rate and a weight array, the last value of which is the threshold value
 function perceptron(inputSize) {
     this.weights = [initweight(), initweight(), initweight(), initweight(), initweight(), initweight(), initweight(), initweight(), initweight()].slice(0, inputSize + 1);
-    this.lRate = .1
+    this.lRate = .1 //learning rate is set by experiment, this is a placeholder
 }
 
 perceptron.prototype = { //methods attached to perceptrons:
-    //calculate returns -1 or 1 for a given input co-ordinate
+    //sigmoid function
     sigmoid: function(a) {
         var m = 1 // slope of sigmoid
         var result = (1 / (1 + Math.exp(a * m * -1)))
         return result
     },
+    //derivitave of sigmoid fn
     sigPrime: function(a) {
         return this.sigmoid(a) * (1 - this.sigmoid(a))
     },
+    //calculate returns the activation of a given node (weights times inputs summed)
     calc: function(inputs) {
-        var sum = _.reduce(_.map(inputs.push(1), (input, key) => input * this.weights[key]), (sum, weightedinput) => sum + weightedinput)
+        var sum = _.reduce(_.map(inputs.concat(1), (input, key) => input * this.weights[key]), (sum, weightedinput) => sum + weightedinput)
         return sum
     },
+    //output is just the activation (calc) put through the sigmoid function 
     output: function(inputs) {
         return this.sigmoid(this.calc(inputs))
     },
+    //update weights, used for hidden nodes
     learn: function(inputs, d, sum) {
         var xi = inputs.concat(1)
 
+        var d1 = this.sigPrime(this.calc(inputs))
         this.weights = _.map(this.weights, (weight, index) => {
-
-            var d1 = this.sigPrime(this.calc(inputs))
-
             return (weight + (d1 * sum * xi[index] * this.lRate)) //xi * c
         })
     },
+    //update weights, used for output nodes
     learnOutput: function(inputs, d) {
         var xi = inputs.concat(1)
+        var d1 = (d - this.output(inputs)) * this.sigPrime(this.calc(inputs))
         this.weights = _.map(this.weights, (weight, index) => {
-            var gamma = this.output(inputs) //
-            var d1 = (d - gamma) * this.sigPrime(this.calc(inputs))
-
             return (weight + (d1 * xi[index] * this.lRate)) //xi * c
         })
     }
 
 }
 
+//initialize 6,2 network
 function network() {
     this.hiddenLayer = [new perceptron(2), new perceptron(2), new perceptron(2), new perceptron(2), new perceptron(2), new perceptron(2)]
     this.outputLayer = [new perceptron(6), new perceptron(6), new perceptron(6)]
 }
 
-network.prototype = { //a function to call update on every node in a layer
-    calcHidden: function(input) {
-        // console.log(this.hiddenLayer[0].output(input[0], input[1]))
-        // console.log(input)
+network.prototype = {
+        //calculates outputs of hidden layer nodes 
+        calcHidden: function(input) {
+            return [
+                this.hiddenLayer[0].output([input[0], input[1]]),
+                this.hiddenLayer[1].output([input[0], input[2]]),
+                this.hiddenLayer[2].output([input[0], input[3]]),
+                this.hiddenLayer[3].output([input[1], input[2]]),
+                this.hiddenLayer[4].output([input[1], input[3]]),
+                this.hiddenLayer[5].output([input[2], input[3]])
+            ]
+        },
+        //calculate ouput of entire network
+        calc: function(input) {
+            var hiddenLayerOutput = this.calcHidden(input)
 
-        return [
-            this.hiddenLayer[0].output([input[0], input[1]]),
-            this.hiddenLayer[1].output([input[0], input[2]]),
-            this.hiddenLayer[2].output([input[0], input[3]]),
-            this.hiddenLayer[3].output([input[1], input[2]]),
-            this.hiddenLayer[4].output([input[1], input[3]]),
-            this.hiddenLayer[5].output([input[2], input[3]])
+            var output = _.map(this.outputLayer, (node) => node.output(hiddenLayerOutput))
+                // console.log(output)
+            return output
+        },
+        //set learning rate for every node
+        setLR: function(newLR) {
+            _.each(this.hiddenLayer.concat(this.outputLayer), (node) => this.lRate = newLR)
+        },
+        //train sets learning rate and then calls learn() for each point in the training dataset 
+        train: function(data) {
+            var desired = parseClass(data[4])
 
-            // this.hiddenLayer[3].output(input[1], input[0]),
-            // this.hiddenLayer[6].output(input[2], input[0]),
-            // this.hiddenLayer[7].output(input[2], input[1]),
-            // this.hiddenLayer[9].output(input[3], input[0]),
-            // this.hiddenLayer[10].output(input[3], input[1]),
-            // this.hiddenLayer[11].output(input[3], input[2])
-        ]
-    },
-    calc: function(input) {
-        var hiddenLayerOutput = this.calcHidden(input)
+            //5% noise added to prevent overfitting
+            var DPwNoise = _.map(data, (val, i) => {
+                    if (i === 4) return val
+                    return (val * (.975 + (Math.random() / 20)))
+                })
+                //calculate hidden layer outputs
+            var hiddenLayerOutput = this.calcHidden(DPwNoise)
 
-        var output = _.map(this.outputLayer, (node) => node.output(hiddenLayerOutput))
-            // console.log(output)
-        return output
-    },
-    //train sets learning rate and then calls learn() for each point in the training dataset 
-    train: function(data) {
-        var desired = parseClass(data[4])
-            // console.log(desired)
+            //feed forward
+            var output = _.map(this.outputLayer, (node) => {
+                return node.output(hiddenLayerOutput)
+            })
 
-        var hiddenLayerOutput = this.calcHidden(data)
+            var error = _.map(desired, (value, i) => {
+                return (value - output[i])
+            })
 
-        var output = _.map(this.outputLayer, (node) => {
-            return node.output(hiddenLayerOutput)
-        })
-        var error = _.map(desired, (value, i) => {
-            return (value - output[i])
-        })
+            // modify output weightights
+            _.each(this.outputLayer, (node, i) => {
+                node.learnOutput(hiddenLayerOutput, desired[i])
+            })
 
-        //modify output weights
-        _.each(this.outputLayer, (node, i) => {
-            node.learnOutput(hiddenLayerOutput, desired[i])
-        })
+            var sums = [0, 0, 0, 0, 0, 0]
 
-        var sums = [0, 0, 0, 0, 0, 0]
-
-        //modify hidden weights
-        for (var o = 0; o < 3; o++) {
-            for (var s = 0; s < 6; s++) {
-                console.log(s,this.outputLayer[s])
-
-                sums[s] += (desired[o] - output[o]) * this.outputLayer[s].sigPrime(this.outputLayer[s].calc([hiddenLayerOutput[s], hiddenLayerOutput[s]])) * this.outputLayer[o].weights[s]
+            // modify hidden weights
+            for (var o = 0; o < 3; o++) {
+                for (var s = 0; s < 6; s++) {
+                    sums[s] += (desired[o] - output[o]) * this.outputLayer[o].sigPrime(this.outputLayer[o].calc([hiddenLayerOutput[s], hiddenLayerOutput[s]])) * this.outputLayer[o].weights[s]
+                }
             }
-        }
 
-        for (var o = 0; o < 3; o++) {
+            for (var o = 0; o < 3; o++) {
+                this.hiddenLayer[2].learn([data[0], data[1]], desired[o], sums[0])
+                this.hiddenLayer[1].learn([data[0], data[2]], desired[o], sums[1])
+                this.hiddenLayer[2].learn([data[0], data[3]], desired[o], sums[2])
+                this.hiddenLayer[3].learn([data[1], data[2]], desired[o], sums[3])
+                this.hiddenLayer[4].learn([data[1], data[3]], desired[o], sums[4])
+                this.hiddenLayer[5].learn([data[2], data[3]], desired[o], sums[5])
+            }
 
-            this.hiddenLayer[0].learn([data[0], data[1]], desired[o], sum[0]),
-                this.hiddenLayer[1].learn([data[2], data[3]], desired[o], sum[1]),
-                this.hiddenLayer[0].learn([input[0], input[1]], desired[o], sum[2]),
-                this.hiddenLayer[1].learn([input[0], input[2]], desired[o], sum[3]),
-                this.hiddenLayer[2].learn([input[0], input[3]], desired[o], sum[4]),
-                this.hiddenLayer[3].learn([input[1], input[2]], desired[o], sum[5]),
-                this.hiddenLayer[4].learn([input[1], input[3]], desired[o], sum[6]),
-                this.hiddenLayer[5].learn([input[2], input[3]], desired[o], sum[7])
-        }
+        },
+        //test takes a datapoint, and returns true if the current network classifies it correctly
+        test: function(data) {
+            var outvector = parseClass(data[4])
 
-    },
-    //test takes a datapoint, and returns true if the current network classifies it correctly
-    test: function(data) {
-        var outvector = parseClass(data[4])
+            var output = this.calc(data.slice(0, 4)) //independent datapoints
 
-        var output = this.calc(data.slice(0, 4))
-            // console.log(_.map(output, (val) => val.toFixed(1)), outvector)
+            output = _.map(output, (o) => { //sets highest output activation to 1, and others to 0 for the purposes of the output vector
+                if (_.max(output) == o)
+                    return 1
+                else
+                    return 0
+            })
 
-        output = _.map(output, (o) => {
-            if (_.max(output) == o)
-                return 1
+            if (_.every(outvector, (value, i) => value == output[i]))
+                return true
             else
-                return 0
-        })
+                return false
 
-        if (_.every(outvector, (value, i) => value == output[i]))
-            return true
-        else
-            return false
+        },
+        //calculates mean square error 
+        totalCost: function(data) {
+            var outvector = parseClass(data[4])
 
-    },
-    totalCost: function(data) {
-        var outvector = parseClass(data[4])
+            var output = this.calc(data.slice(0, 4))
 
-        var output = this.calc(data.slice(0, 4))
+            return _.reduce(_.map(outvector, (val, i) => Math.exp(val - output[i], 2)), (v, sum) => v + sum)
 
-        return _.reduce(_.map(outvector, (val, i) => Math.exp(val - output[i], 2)), (v, sum) => v + sum)
-
-        // if (_.every(outvector, (value, i) => value == output[i]))
-        // return true
-        // else
-        // return false
+        }
 
     }
-
-
-}
-
-
+    //converts class to a 1-hot encoded vector
 function parseClass(classString) {
     var output = [0, 0, 0]
 
@@ -214,59 +186,40 @@ function parseClass(classString) {
     return output
 }
 
-//a global structure to keep track of current active perceptrons
-
 //experiment trains and tests networks iterativly with a given training method. once error is 0, it records some information out to the html
 function experiment() {
-    // document.write('<h4>' + method + ':</h4>');
+    document.write('<h4> i: iteration<br>Lrate: learning rate(decays) <br> error: # of incorrectly classified datapoints <br>MSE: Mean Squared Error <br> ΔMSE: delta MSE </h4>');
     var ErrorProgress = ""
     var net = new network();
-    // var start = new Date();
-    for (var i = 0; i < 15; i++) {
+    var start = new Date();
+    var lr = 1
+    var lastMSE = _.reduce(_.map(trainingData, datapoint => net.totalCost(datapoint)), (v, sum) => v + sum) / trainingData.length
+
+    for (var i = 0; i < 500; i++) {
+        lr = lr * .998
+        net.setLR(lr)
 
         _.each(trainingData, (datapoint) => net.train(datapoint))
-            // console.log(i)
-            // console.log(trainingData.length, i)
+
         var numCorrect = _.filter(trainingData, (datapoint) => net.test(datapoint)).length
 
-        var totalCost = _.reduce(_.map(trainingData, datapoint => net.totalCost(datapoint)), (v, sum) => v + sum)
+        var totalCost = _.reduce(_.map(trainingData, datapoint => net.totalCost(datapoint)), (v, sum) => v + sum) / trainingData.length
+        var deltaMSE = totalCost - lastMSE
+        lastMSE = totalCost
+        if (i % 10 === 0)
+            ErrorProgress += ("i: " + i + "\t\tLrate: " + lr.toFixed(2) + "\t\terror: " + (trainingData.length - numCorrect) + "\t\tMSE: " + totalCost.toFixed(3) + "\t\tΔMSE: " + deltaMSE.toFixed(5) + "<br> ")
 
-        ErrorProgress += ("iteration: " + i + " error: " + (trainingData.length - numCorrect) + "  errorVal: " + totalCost.toFixed(3) + "<br> ")
         if (numCorrect == trainingData.length)
             break
 
     }
-    // var finish = new Date();
-    // var difference = new Date();
-    // difference.setTime(finish.getTime() - start.getTime());
+    var finish = new Date();
+    var difference = new Date();
+    difference.setTime(finish.getTime() - start.getTime());
 
 
-    // render(network)
     document.write(ErrorProgress);
-    // document.write('<h4> Final Score: ' + (_.filter(trainingData, test).length + '/' + trainingData.length) + '</h4>')
-    // document.write('<h4> Final weights: ' + network[0].weights + '<br>' + network[1].weights + '</h4>')
-    // document.write('Took ' + difference.getMilliseconds() + "ms")
+    document.write('Took ' + difference.getMilliseconds() + "ms")
 }
 
 experiment()
-
-//render writes out a grid of values with different unicode values correspoding to different output values
-// function render(network) {
-// document.write('<p class="display">');
-
-// for (var y = 20; y > -20; y -= 1) { //range of y values to display 
-// for (var x = -20; x < 20; x += 1) { //range of x values to display
-// if ((network[0].calc(x, y) === 1) && (network[1].calc(x, y) === 1))
-// var chr = "&#9632;" //unicode for black square
-// else if (network[0].calc(x, y) === 1)
-// var chr = "1"
-// else if (network[1].calc(x, y) === 1)
-// var chr = "0"
-// else var chr = "&#9633;" //unicode for white square
-// document.write(chr);
-// }
-// document.write("<br>");
-// }
-// document.write("</p>");
-// document.write("<br>");
-// }
